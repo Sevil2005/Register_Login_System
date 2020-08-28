471cd85551fad12a82e33a935d285256de700fdc
Sevil Rasulova <68187750+Sevil2005@users.noreply.github.com>
2020-08-28 23:24:10 +0400

    Update additional_funcs.py

--- additional_funcs.py
+++ additional_funcs.py
@@ -80,13 +80,13 @@ def changeData(student_psw):
             user.email = new_email
 
             new_telNumber = input("Yeni əlaqə nömrənizi daxil edin: +994")
-            while new_telNumber.isdigit() == False and len(new_telNumber) != 9:
+            while new_telNumber.isdigit() == False or len(new_telNumber) != 9:
                 print("Əlaqə nömrəniz 9 rəqəmli olmalıdır!(nümunə:501234567)")
                 new_telNumber = input("Yeni əlaqə nömrənizi daxil edin: +994")
             user.telNumber = new_telNumber
 
             new_password = input("Yeni şifrənizi yazın: ")
-            while new_password.isdigit() == False and len(new_password) != 3:
+            while new_password.isdigit() == False or len(new_password) != 3:
                 print("Şifrə 3 rəqəmli ədəd olmalıdır!")
                 new_password = input("Yeni şifrənizi yazın: ")
             user.password = new_password
