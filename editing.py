f5cc77197c7e517b4c5b0142fcb54ad164044bd6
Sevil Rasulova <68187750+Sevil2005@users.noreply.github.com>
2020-08-28 20:24:45 +0400

    Update editing.py

--- editing.py
+++ editing.py
@@ -1,9 +1,8 @@
 import checking_funcs
+import additional_funcs
 
 
 # Tələbə class-ının yaradılması və listə yığılması:
-
-student = []
 
 
 class Student:
@@ -39,7 +38,7 @@ def registering_system():
             checking_funcs.checking_password(password)
             s = Student(checking_funcs._name, checking_funcs._surname,
                         checking_funcs._email, checking_funcs._telNumber, checking_funcs._password)
-            student.append(s)
+            additional_funcs.student.append(s)
     else:
         print("Tələbə sayı rəqəm vəya ədəd olmalıdır!")
 
@@ -48,120 +47,6 @@ print("Tələbələrin datalarının sistemə yerləşdirilmə prosesi sona çat
 
 
 registering_system()
-
-
-# Tələbə adına görə tələbə məlumatlarının göstərilmə funksiyası:
-
-
-def showData(student_name):
-    i = 1
-    for user in student:
-        if student_name == user.name:
-            print(f"Student{i}")
-            print(user.name)
-            print(user.surname)
-            print(user.email)
-            print(user.telNumber)
-            i += 1
-            break
-        else:
-            pass
-    else:
-        print("Bu adda tələbə yoxdu!")
-
-
-# Tələbələrin siyahısının göstərilmə funksiyası:
-
-
-def show_data():
-    a = 1
-    for user in student:
-        user.show(a)
-        a += 1
-
-# Bütün tələbə məlumatlarının göstərilmə funksiyası:
-
-
-def allData():
-    i = 1
-    for user in student:
-        print(f"Student{i}")
-        print(user.name)
-        print(user.surname)
-        print(user.email)
-        print(user.telNumber)
-        print(user.password)
-        i += 1
-
-
-# Tələbə koduna görə tələbə məlumatlarının dəyişdirilmə funksiyası:
-
-def changeData(student_psw):
-    for user in student:
-        if student_psw == user.password:
-            while True:
-                new_name = input("Yeni adınızı daxil edin: ")
-                if new_name.isalpha():
-                    user.name = new_name
-                    break
-                else:
-                    print(
-                        "Adınızda ancaq əlifbanın hərflərindən istifadə edə bilərsiz!")
-
-            while True:
-                new_surname = input("Yeni soyadınızı daxil edin: ")
-                if new_surname.isalpha():
-                    user.surname = new_surname
-                    break
-                else:
-                    print(
-                        "Soyadınızda ancaq əlifbanın hərflərindən istifadə edə bilərsiz!")
-
-            while True:
-                new_email = input("Yeni email adresinizi daxil edin: ")
-                if "@" in new_email:
-                    user.email = new_email
-                    break
-                else:
-                    print("Yeni emailiniz standartlara uyğun deyil!")
-
-            while True:
-                new_telNumber = input("Yeni əlaqə nömrənizi daxil edin: +994")
-                if new_telNumber.isdigit() and len(new_telNumber) == 9:
-                    user.telNumber = new_telNumber
-                    break
-                else:
-                    print("Əlaqə nömrəniz 9 rəqəmli olmalıdır!(nümunə:501234567)")
-
-            while True:
-                new_password = input("Yeni şifrənizi yazın: ")
-                if new_password.isdigit() and len(new_password) == 3:
-                    user.password = new_password
-                    print("Qeydiyyatınız uğurla başa çatdı!")
-                    break
-                else:
-                    print("Şifrə 3 rəqəmli ədəd olmalıdır!")
-
-            showData(new_name)
-            break
-        else:
-            pass
-    else:
-        print("Sistemdə bu kodlu tələbə yoxdur!")
-
-# Tələbə koduna görə tələbə məlumatlarının silinmə funksiyası:
-
-
-def deleteData(student_psw):
-    for user in student:
-        if student_psw == user.password:
-            student.remove(user)
-            print("Daxil edilən koda uyğun tələbənin dataları sistemdən silindi!")
-            break
-        else:
-            pass
-    else:
-        print("Sistemdə bu kodlu tələbə yoxdur!")
 
 
 # Outputda görmək istədiyiniz əmrlərin verilməsi:
@@ -174,26 +59,26 @@ for i in range(100):
                     Hansısa tələbənin datasını sistemdən silmək istəyirsizsə-5 
                     ==>''')
     if command == "1":
-        show_data()
+        additional_funcs.show_data()
     elif command == "2":
         name_input = input(
             "Datasını görmək istədiyiniz tələbənin adını daxil edin: ")
-        showData(name_input)
+        additional_funcs.showData(name_input)
     elif command == "3":
         while True:
             secret_psw = input("4-rəqəmli gizli şifrəni daxil edin: ")
             if secret_psw == "2005":
-                allData()
+                additional_funcs.allData()
                 break
             else:
                 print("Yanlış şifrə!")
     elif command == "4":
         psw_input = input(
             "Datasını dəyişmək istədiyiniz tələbənin kodunu daxil edin: ")
-        changeData(psw_input)
+        additional_funcs.changeData(psw_input)
     elif command == "5":
         psw_input = input(
             "Datasını silmək istədiyiniz tələbənin kodunu daxil edin: ")
-        deleteData(psw_input)
+        additional_funcs.deleteData(psw_input)
     else:
         print("Belə funksionallıq yoxdur!Əldə etmək istədiyiniz məlumatlara uyğun düzgün ədədi daxil edin!")
