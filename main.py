dafde64b4a72d3be62f13c9bc2efbae6425a24aa
Sevil Rasulova <68187750+Sevil2005@users.noreply.github.com>
2020-08-29 12:59:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,47 +2,6 @@ import checking_funcs
 import additional_funcs
 import json
 
-# Tələbə məlumatlarının dict-ə yığılması və 'student_db.json' faylına köçürülməsi:
-
-
-class dictGenerator(dict):
-    def __init__(self):
-        self = dict()
-
-    def addKeyValue(self, key, value):
-        self[key] = value
-
-
-data = dictGenerator()
-data.addKeyValue('students', [])
-
-
-def createDict(_name, _surname, _email, _telNumber, _password):
-    innerDict = dictGenerator()
-    innerDict.addKeyValue('name', _name)
-    innerDict.addKeyValue('surname', _surname)
-    innerDict.addKeyValue('email', _email)
-    innerDict.addKeyValue('telNumber', _telNumber)
-    innerDict.addKeyValue('password', _password)
-    return innerDict
-
-
-# Tələbə class-ının yaradılması və listə yığılması:
-
-
-class Student:
-    def __init__(self, name, surname, email, telNumber, password):
-        self.name = name
-        self.surname = surname
-        self.email = email
-        self.telNumber = telNumber
-        self.password = password
-
-    def show(self, a):
-        print(
-            f"Tələbə{a} adı : {self.name} / soyadı: {self.surname} / email: {self.email} / telefon nömrəsi: +994{self.telNumber}.")
-
-
 # Tələbənin datalarının daxil edilmə prosesi:
 
 
@@ -62,11 +21,8 @@ def registering_system():
             checking_funcs.checking_telNumber(telNumber)
             password = input("Şifrənizi yazın: ")
             checking_funcs.checking_password(password)
-            s = Student(checking_funcs._name, checking_funcs._surname,
-                        checking_funcs._email, checking_funcs._telNumber, checking_funcs._password)
-            additional_funcs.student.append(s)
-            data['students'].append(createDict(checking_funcs._name, checking_funcs._surname,
-                                               checking_funcs._email, checking_funcs._telNumber, checking_funcs._password))
+            additional_funcs.data['students'].append(additional_funcs.createDict(checking_funcs._name, checking_funcs._surname,
+                                                                                 checking_funcs._email, checking_funcs._telNumber, checking_funcs._password))
     else:
         print("Tələbə sayı rəqəm vəya ədəd olmalıdır!")
 
@@ -74,25 +30,22 @@ def registering_system():
 registering_system()
 print("Tələbələrin datalarının sistemə yerləşdirilmə prosesi sona çatdı!")
 with open("student_db.json", "w") as connect:
-    json.dump(data, connect)
+    json.dump(additional_funcs.data, connect)
 
 # Outputda görmək istədiyiniz əmrlərin verilməsi:
 command = input('''Həyata keçirmək istədiyiniz funksionallığa uyğun rəqəmi daxil edin:
-                    Tələbələrin ümumi siyahısını görmək istəyirsinizsə => 1
-                    Hansısa tələbənin datasını əldə etmək istəyirsizsə => 2
-                    Bütün tələbələrin datasını əldə etmək istəyirsizsə => 3
-                    Hansısa tələbənin datasını dəyişmək istəyirsizsə => 4
-                    Hansısa tələbənin datasını sistemdən silmək istəyirsizsə => 5
+                    Hansısa tələbənin datasını əldə etmək istəyirsizsə => 1
+                    Bütün tələbələrin datasını əldə etmək istəyirsizsə => 2
+                    Hansısa tələbənin datasını dəyişmək istəyirsizsə => 3
+                    Hansısa tələbənin datasını sistemdən silmək istəyirsizsə => 4
                     Əgər sistemdən çixmaq istəyirsinizsə bunu yazın => exit  
                     ==>''')
 while command != "exit":
     if command == "1":
-        additional_funcs.show_data()
-    elif command == "2":
         name_input = input(
-            "Datasını görmək istədiyiniz tələbənin adını daxil edin: ")
+            "datasını görmək istədiyiniz tələbənin adını daxil edin: ")
         additional_funcs.showData(name_input)
-    elif command == "3":
+    elif command == "2":
         while True:
             secret_psw = input("4-rəqəmli gizli şifrəni daxil edin: ")
             if secret_psw == "2005":
@@ -100,22 +53,21 @@ while command != "exit":
                 break
             else:
                 print("Yanlış şifrə!")
+    elif command == "3":
+        psw_input = input(
+            "datasını dəyişmək istədiyiniz tələbənin kodunu daxil edin: ")
+        additional_funcs.changeData(psw_input)
     elif command == "4":
         psw_input = input(
-            "Datasını dəyişmək istədiyiniz tələbənin kodunu daxil edin: ")
-        additional_funcs.changeData(psw_input)
-    elif command == "5":
-        psw_input = input(
-            "Datasını silmək istədiyiniz tələbənin kodunu daxil edin: ")
+            "datasını silmək istədiyiniz tələbənin kodunu daxil edin: ")
         additional_funcs.deleteData(psw_input)
     else:
         print("Belə funksionallıq yoxdur!Əldə etmək istədiyiniz məlumatlara uyğun düzgün ədədi daxil edin!")
 
     command = input('''Həyata keçirmək istədiyiniz funksionallığa uyğun rəqəmi daxil edin:
-                    Tələbələrin ümumi siyahısını görmək istəyirsinizsə => 1
-                    Hansısa tələbənin datasını əldə etmək istəyirsizsə => 2
-                    Bütün tələbələrin datasını əldə etmək istəyirsizsə => 3
-                    Hansısa tələbənin datasını dəyişmək istəyirsizsə => 4
-                    Hansısa tələbənin datasını sistemdən silmək istəyirsizsə => 5
+                    Hansısa tələbənin datasını əldə etmək istəyirsizsə => 1
+                    Bütün tələbələrin datasını əldə etmək istəyirsizsə => 2
+                    Hansısa tələbənin datasını dəyişmək istəyirsizsə => 3
+                    Hansısa tələbənin datasını sistemdən silmək istəyirsizsə => 4
                     Əgər sistemdən çixmaq istəyirsinizsə bunu yazın => exit  
                     ==>''')
